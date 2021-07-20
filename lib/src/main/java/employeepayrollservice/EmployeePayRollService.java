583a926fb8322918e16f179f21c7e4d12dd8f23c
lavanya-pg <lavanyapg8@gmail.com>
2021-07-20 14:50:35 +0530

    Initial Commit: Welcome To the EmployeePayRoll Service Program

---
+++ lib/src/main/java/employeepayrollservice/EmployeePayRollService.java
@@ -0,0 +1,10 @@
+package employeepayrollservice;
+
+
+public class EmployeePayRollService 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome To The Employee PayRoll Service");
+	}
+}
